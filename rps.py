bce48c53f189db557d36523eba31bd2fa9f3c384
Vinay <saivinaybellamkonda@gmail.com>
2020-11-09 23:04:33 +0530

    adjusted UI elements

--- rps.py
+++ rps.py
@@ -7,17 +7,17 @@ model = tf.keras.models.load_model("models/rps_model")
 
 vid = cv2.VideoCapture(0)
 
-classes = ['rock', 'paper', 'scissors']
-prev = ''
+classes = ["rock", "paper", "scissors"]
+prev = ""
 
-rock = cv2.imread('assets/rock.jpg')
-paper = cv2.imread('assets/paper.jpg')
-scissor = cv2.imread('assets/scissor.jpg')
+rock = cv2.imread("assets/rock.png")
+paper = cv2.imread("assets/paper.png")
+scissor = cv2.imread("assets/scissors.png")
 
 while True:
     ret, frame = vid.read()
-    cv2.rectangle(frame, (0, 0), (256, 256), (255, 255, 255), 2)
-    img = frame[0:256, 0:256]
+    cv2.rectangle(frame, (40, 40), (296, 296), (0, 255, 0), 2)
+    img = frame[40:296, 40:296]
     img = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
     img = cv2.resize(img, (100, 100))
     if cv2.waitKey(1) & 0xFF == ord("q"):
@@ -28,8 +28,29 @@ while True:
     move = classes[np.argmax(p)].upper()
     if move != prev:
         comp_move = choice([0, 1, 2])
-    cv2.putText(frame, "prediction: "+move, (25, 25), cv2.QT_FONT_NORMAL, 0.5, (255, 0, 0), 1, cv2.LINE_AA)
-    frame[0:256, -256:] = rock if comp_move == 0 else paper if comp_move == 1 else scissor
+    cv2.putText(
+        frame,
+        "prediction: " + move,
+        (85, 25),
+        cv2.QT_FONT_NORMAL,
+        0.5,
+        (255, 0, 0),
+        1,
+        cv2.LINE_AA,
+    )
+    cv2.putText(
+        frame,
+        "CPU move: " + classes[comp_move],
+        (410, 25),
+        cv2.QT_FONT_NORMAL,
+        0.5,
+        (255, 0, 0),
+        1,
+        cv2.LINE_AA,
+    )
+    frame[40:296, -296:-40] = (
+        rock if comp_move == 0 else paper if comp_move == 1 else scissor
+    )
     prev = move
     cv2.imshow("Rock Paper Scissors", frame)
 vid.release()
